bec5b969b8d8234595f4b242f8fbf76b6c3454cc
Senne Vansant <r0894963@student.thomasmore.be>
2024-04-06 13:02:27 +0200

    add project

---
+++ Portfolio/Portfolio.DAL/DAL/Context/PortolioContext.cs
@@ -0,0 +1,43 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Context
+{
+    public interface IPortfolioContext
+    {
+        DbSet<Project> Projects { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Framework> Frameworks { get; set; }
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken token);
+
+    }
+    public class PortolioContext : DbContext, IPortfolioContext
+    {
+    
+        public PortolioContext()
+        {
+
+            
+        }
+        public PortolioContext(DbContextOptions<PortolioContext> options) : base(options) 
+        { 
+        
+        
+        }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Framework> Frameworks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
